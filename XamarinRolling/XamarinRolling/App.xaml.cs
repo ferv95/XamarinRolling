a66e7d1338f2f72a871e5162f8ee7f6d3ca95e8e
Fernando Vado Marín <fernandovado95@gmail.com>
2018-05-27 23:21:50 +0200

    Revert "Revert "implementacion de login""
    This reverts commit 80d2fa58ee5a136101caed8d77d2f3b93ba9398d.

--- XamarinRolling/XamarinRolling/App.xaml.cs
+++ XamarinRolling/XamarinRolling/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XamarinRolling
 		{
 			InitializeComponent();
 
-			MainPage = new Views.MainAutoescuelaView();
+			MainPage = new Views.LoginPage();
 		}
 
 		protected override void OnStart ()
