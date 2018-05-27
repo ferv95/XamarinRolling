327400614951f56f71a1ece73094093f3a2cfa8c
Isidoro López Rodríguez <isidorolopezrodriguez@hotmail.com>
2018-05-27 20:00:29 +0200

    Todos los insertar funcionales

---
+++ XamarinRolling/XamarinRolling/ViewModels/InsertarAdministradorViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using XamarinRolling.Helpers;
+using XamarinRolling.Models;
+using XamarinRolling.ViewModels.Base;
+
+namespace XamarinRolling.ViewModels
+{
+    public class InsertarAdministradorViewModel : ViewModelBase
+    {
+        public InsertarAdministradorViewModel()
+        {
+            this.Administrador = new Plantilla();
+        }
+
+        private Plantilla _Administrador;
+
+        public Plantilla Administrador
+        {
+            get { return this._Administrador; }
+            set
+            {
+                this._Administrador = value;
+                OnPropertyChanged("Administrador");
+            }
+        }
+
+        HelperAutoescuelaAzure helper = new HelperAutoescuelaAzure();
+
+        public Command InsertarAdministrador
+        {
+            get
+            {
+                return new Command(async () => {
+                    Administrador.Image = "avatar.png";
+                    Administrador.Rol = "ADMINISTRACION";
+                    if (Administrador.Codigo == 0)
+                    {
+                        Administrador = null;
+                    }
+                    else
+                    {
+                        Plantilla otro = await helper.GetAdministrador(Administrador.Codigo);
+                        if (otro != null)
+                        {
+                            Administrador = null;
+                        }
+                        else
+                        {
+                            await helper.CrearProfesor(this.Administrador);
+                            await Application.Current.MainPage.Navigation.PopModalAsync();
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ XamarinRolling/XamarinRolling/Views/InsertarAdministradorView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinRolling.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class InsertarAdministradorView : ContentPage
+	{
+		public InsertarAdministradorView ()
+		{
+			InitializeComponent ();
+            txtseccion.Text = "";
+            txtcodigo.Text = "";
+            txttelef.Text = "";
+        }
+	}
+}
