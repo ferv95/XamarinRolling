e0bd3d78b02cec025ae0ba000f20c47e70a99f9a
Isidoro López Rodríguez <isidorolopezrodriguez@hotmail.com>
2018-05-28 09:45:00 +0200

    imagen por defecto en perfil

--- XamarinRolling/XamarinRolling/Views/MiPerfilView.xaml.cs
+++ XamarinRolling/XamarinRolling/Views/MiPerfilView.xaml.cs
@@ -1,20 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinRolling.ViewModels;
 
 namespace XamarinRolling.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MiPerfilView : ContentPage
 	{
 		public MiPerfilView ()
 		{
 			InitializeComponent ();
+            PerfilViewModel viewModel = (PerfilViewModel)BindingContext;
+            if (viewModel.UsuarioPerfil.Image == null)
+            {
+                viewModel.UsuarioPerfil.Image = "avatar.png";
+            }
 		}
 	}
 }
